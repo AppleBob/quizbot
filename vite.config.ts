4187df8f646f6c352a18362c527e5a757c965c20
AppleBob <aidenliu08@gmail.com>
2025-11-15 22:37:18 +0800

    more fix

--- vite.config.ts
+++ vite.config.ts
@@ -22,9 +22,10 @@ export default defineConfig({
   root: path.resolve(import.meta.dirname, "client"),
   publicDir: path.resolve(import.meta.dirname, "client", "public"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
   },
+
   server: {
     host: true,
     allowedHosts: [
